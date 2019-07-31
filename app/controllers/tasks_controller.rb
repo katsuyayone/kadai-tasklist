e2bc5caa6929c206266e27a8882c1dbb91d9e2c3
katsuya.yone <seiryo.soccer.ky@i.softbank.jp>
2019-07-31 13:39:09 +0000

    complete kadai

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:edit, :destroy]
+  before_action :correct_user, only: [:show, :edit, :destroy]
   
   def index
     if logged_in?
@@ -53,6 +53,7 @@ class TasksController < ApplicationController
   
   def set_task
     @task = Task.find(params[:id])
+    
   end
   
   def task_params
